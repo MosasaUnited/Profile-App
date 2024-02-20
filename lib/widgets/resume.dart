5ebb22e4be3e6a984cd2aa1b6fe6c9977ebd0230
MosasaUnited <mostafa447@hotmail.com>
2024-02-20 02:22:29 +0200

    MakeResumeAs A Responsive UI

--- lib/widgets/resume.dart
+++ lib/widgets/resume.dart
@@ -8,8 +8,6 @@ class Resume extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
-    final screenHeight =
-        MediaQuery.of(context).size.height; // Corrected height access
 
     return Scaffold(
       appBar: AppBar(
