178147ec6c2ae0d01f5a044162269b2a6648ffd3
Mostafa Mahmoud Saad <79766907+MosasaUnited@users.noreply.github.com>
2024-08-10 14:21:41 +0300

    AddTitleTextAnimationWidgetToProjectsFeature

--- lib/features/projects/presentation/screens/projects_screen_body.dart
+++ lib/features/projects/presentation/screens/projects_screen_body.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../widgets/title_text.dart';
 
 class ProjectsScreenBody extends StatelessWidget {
   const ProjectsScreenBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        const TitleText(),
+        TitleText(
+          title: 'Latest',
+          prefix: 'Projects',
+        ),
         SizedBox(
           height: 50.h,
         ),
---
+++ lib/features/projects/presentation/widgets/title_text.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class TitleText extends StatelessWidget {
+  const TitleText({super.key, required this.prefix, required this.title});
+
+  final String prefix;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          prefix,
+          style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                color: Colors.white,
+                fontSize: 20.sp,
+                fontWeight: FontWeight.w900,
+              ),
+        ),
+        kIsWeb
+            ? ShaderMask(
+                shaderCallback: (bounds) {
+                  return const LinearGradient(
+                      end: Alignment.centerRight,
+                      begin: Alignment.centerLeft,
+                      colors: [
+                        Colors.pink,
+                        Colors.cyanAccent,
+                      ]).createShader(bounds);
+                },
+                child: Text(
+                  title,
+                  style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                        color: Colors.white,
+                        fontSize: 20.sp,
+                        fontWeight: FontWeight.w900,
+                      ),
+                ),
+              )
+            : Text(
+                title,
+                style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                      color: Colors.white,
+                      fontSize: 30.sp,
+                      fontWeight: FontWeight.w900,
+                    ),
+              ),
+      ],
+    );
+  }
+}
