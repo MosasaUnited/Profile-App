4b458ef21193b31372fbf645121228f9922d3017
Mostafa Mahmoud Saad <79766907+MosasaUnited@users.noreply.github.com>
2024-10-07 21:38:35 +0300

    Add Single Child To Projects Column

--- lib/features/projects/presentation/widgets/project_deatail.dart
+++ lib/features/projects/presentation/widgets/project_deatail.dart
@@ -11,50 +11,52 @@ class ProjectDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.sizeOf(context);
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Align(
-          alignment: Alignment.topCenter,
-          child: Text(
-            projectList[index].name,
-            style: Theme.of(context)
-                .textTheme
-                .headlineSmall!
-                .copyWith(color: Colors.white, fontWeight: FontWeight.bold),
-            maxLines: 1,
-            // overflow: TextOverflow.ellipsis,
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Align(
+            alignment: Alignment.topCenter,
+            child: Text(
+              projectList[index].name,
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineSmall!
+                  .copyWith(color: Colors.white, fontWeight: FontWeight.bold),
+              maxLines: 1,
+              // overflow: TextOverflow.ellipsis,
+            ),
           ),
-        ),
-        Responsive.isMobile(context)
-            ? SizedBox(
-                height: 20.h / 2,
-              )
-            : SizedBox(
-                height: 20.h,
-              ),
-        Text(
-          projectList[index].description,
-          style: const TextStyle(color: Colors.grey, height: 1.5),
-          maxLines: size.width > 700 && size.width < 750
-              ? 3
-              : size.width < 470
-                  ? 2
-                  : size.width > 600 && size.width < 700
-                      ? 6
-                      : size.width > 900 && size.width < 1060
-                          ? 6
-                          : 4,
-          overflow: TextOverflow.ellipsis,
-        ),
-        const Spacer(),
-        ProjectLinks(
-          index: index,
-        ),
-        SizedBox(
-          height: 20.0.h / 2,
-        ),
-      ],
+          Responsive.isMobile(context)
+              ? SizedBox(
+                  height: 20.h / 2,
+                )
+              : SizedBox(
+                  height: 20.h,
+                ),
+          Text(
+            projectList[index].description,
+            style: const TextStyle(color: Colors.grey, height: 1.5),
+            maxLines: size.width > 700 && size.width < 750
+                ? 3
+                : size.width < 470
+                    ? 2
+                    : size.width > 600 && size.width < 700
+                        ? 6
+                        : size.width > 900 && size.width < 1060
+                            ? 6
+                            : 4,
+            overflow: TextOverflow.ellipsis,
+          ),
+          const Spacer(),
+          ProjectLinks(
+            index: index,
+          ),
+          SizedBox(
+            height: 20.0.h / 2,
+          ),
+        ],
+      ),
     );
   }
 }
